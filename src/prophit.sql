78303c5058749d12b344391404f6c92803b9ef73
fede-simo <simonettofede@gmail.com>
2026-01-01 19:27:47 +0100

    Correzione sql

--- src/prophit.sql
+++ src/prophit.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS Utente;
-DROP TABLE IF EXISTS Corso;
-DROP TABLE IF EXISTS Acquisto;
 DROP TABLE IF EXISTS Recensione;
+DROP TABLE IF EXISTS Acquisto;
+DROP TABLE IF EXISTS Corso;
+DROP TABLE IF EXISTS Utente;
 
 
 CREATE TABLE Utente (
-    id UNSIGNED INT NOT NULL,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     nome VARCHAR(30) NOT NULL,
     cognome VARCHAR(30) NOT NULL,
     email VARCHAR(30) NOT NULL,
@@ -16,15 +16,13 @@ CREATE TABLE Utente (
 );
 
 
-CREATE TYPE mode AS ENUM('aula', 'online_live', 'online_reg');
-
 CREATE TABLE Corso(
-    id UNSIGNED INT NOT NULL,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     titolo VARCHAR(100) NOT NULL,
     categoria VARCHAR(30) NOT NULL,
     durata INT NOT NULL, 
     costo INT NOT NULL,
-    modalita mode NOT NULL,
+    modalita ENUM('aula', 'online_live', 'online_reg') NOT NULL,
     breve_desc VARCHAR(200) NOT NULL,
     desc_completa VARCHAR(10000) NOT NULL,
     PRIMARY KEY(id)
@@ -32,8 +30,8 @@ CREATE TABLE Corso(
 
 
 CREATE TABLE Acquisto(
-    id_user INT NOT NULL,
-    id_corso INT NOT NULL,
+    id_user INT UNSIGNED NOT NULL,
+    id_corso INT UNSIGNED NOT NULL,
     data DATE NOT NULL,
     FOREIGN KEY (id_user) REFERENCES Utente(id) ON DELETE CASCADE,
     FOREIGN KEY (id_corso) REFERENCES Corso(id) ON DELETE CASCADE,
@@ -42,12 +40,12 @@ CREATE TABLE Acquisto(
 
 
 CREATE TABLE Recensione(
-    id_user INT NOT NULL,
-    id_corso INT NOT NULL,
-    rating REAL NOT NULL,
-    desc VARCHAR(1000),
-    FOREIGN KEY (id_user) REFERENCES Acquisto(id_user) ON DELETE CASCADE,
-    FOREIGN KEY (id_corso) REFERENCES Acquisto(id_corso) ON DELETE CASCADE,
-    PRIMARY KEY(id_user, id_corso);
-)
+    id_user INT UNSIGNED NOT NULL,
+    id_corso INT UNSIGNED NOT NULL,
+    rating DECIMAL(2,1) NOT NULL,
+    descrizione VARCHAR(1000),
+    FOREIGN KEY (id_user) REFERENCES Utente(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_corso) REFERENCES Corso(id) ON DELETE CASCADE,
+    PRIMARY KEY(id_user, id_corso)
+);
 
